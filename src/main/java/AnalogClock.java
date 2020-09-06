afaf08d0d102c54898ccd610775565d6e1cbb4ac
Jacqueline Balanovsky <jacqueli.2309858@nv.ccsd.net>
2020-09-06 23:17:18 +0000

    testing AnalogClock

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -5,9 +5,13 @@ class AnalogClock {
 
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
+        int time = scanner.nextInt();
 
         //Reads ints from user
+        int minutesAfterHour = time % 12;
+        int degrees = minutesAfterHour * 12;
 
+        System.out.println(degrees);
 
         /*
          *  your code goes here
