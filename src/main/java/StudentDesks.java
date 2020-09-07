dbc029513c9299fa0b5481589a646806db9af3ea
Jacqueline Balanovsky <jacqueli.2309858@nv.ccsd.net>
2020-09-07 00:14:49 +0000

    test StudentDesks and AnalogClock

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,11 +7,11 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
+        int class1 = scanner.nextInt();
+        int class2 = scanner.nextInt();
+        int class3 = scanner.nextInt();
         
-
-        /*
-         *  your code goes here
-         */
+        System.out.println((class1 / 2) + (class1 % 2) + + (class2 / 2) + (class2 % 2)+ (class3 % 2) + (class3 / 2));
 
 
         // closing the scanner object
